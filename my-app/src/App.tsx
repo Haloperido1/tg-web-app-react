e8ccbdcf48145f9aa404dd6c32d01177501fbcdc
haloperidol <edvin.ganbar@gmail.com>
2022-10-11 21:07:06 +0300

    init commit

---
+++ my-app/src/App.tsx
@@ -0,0 +1,21 @@
+import React, {useEffect} from 'react';
+import './App.css';
+const tg = window.Telegram.WebApp
+function App() {
+
+    useEffect(() => {
+        tg.ready()
+    }, [])
+
+    const onClose = () => {
+        tg.close()
+    }
+  return (
+    <div className="App">
+      work
+        <button onClick={onClose}>Close</button>
+    </div>
+  );
+}
+
+export default App;
